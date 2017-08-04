00c4efb46fab3eed41575c31b66186ef8897c66d
Jian-Min-Huang <yfr.huang@hotmail.com>
2017-08-04 17:21:57 +0800

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,60 @@
-var fileinclude = require('gulp-file-include'),
-    gulp = require('gulp'),
-    less = require('gulp-less');
+var gulp	= require("gulp"),
+    fileinclude	= require("gulp-file-include"),
+    less	= require("gulp-less"), 
+    concat	= require("gulp-concat"),
+    uglify	= require("gulp-uglify"),
+    rename	= require("gulp-rename");
 
-gulp.task('fileinclude', function() {
-    gulp.src(['./pages/*.html'])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(gulp.dest('./dist'));
-});
+var SRC = "./src";
+var DEST = "./build";
 
-gulp.task('less', function() {
-    gulp.src('./style/less/*.less')
+var JsFileDependency = [ 
+    "./vendors/jquery/jquery.min.js",
+    "./vendors/bootstrap/js/bootstrap.min.js",
+    "./vendors/metisMenu/metisMenu.min.js",
+    "./src/js/*.js"
+];
+
+var CssFileDependency = [ 
+    "./vendors/bootstrap/css/bootstrap.min.css",
+    "./vendors/font-awesome/css/font-awesome.min.css",
+    "./vendors/metisMenu/metisMenu.min.css"
+];
+
+gulp.task("less", function() {
+    gulp.src(SRC + "/less/*.less")
         .pipe(less())
-        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest(DEST + "/css"))
 });
 
+gulp.task("fileinclude", function() {
+    gulp.src(SRC + "/html/*.html")
+        .pipe(fileinclude({
+            prefix: "@@",
+            basepath: "@file"
+        }))
+        .pipe(gulp.dest(DEST));
+});
 
-gulp.task('default',['less','fileinclude']);
+gulp.task('scripts', function() {
+    gulp.src(JsFileDependency)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task('styles', function() {
+    gulp.src(CssFileDependency)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task('scripts:build', function() {
+    gulp.src(jsFileDependency)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest(DEST))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task("default",["less","fileinclude","scripts","styles"]);
+
