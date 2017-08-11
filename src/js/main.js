ea3eb27a3c9023e40cf1be7d2f638ef5ba1ba79f
Jian-Min-Huang <yfr.huang@hotmail.com>
2017-08-11 09:53:34 +0800

    update js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 var notes = [];
-notes.push({path: "../content/Note/Coding_Note.txt", text: "Coding Note"});
-notes.push({path: "../content/Note/Git_Settings.txt", text: "Git Settings"});
-notes.push({path: "../content/Note/MAC_Install.txt", text: "MAC Install"});
-notes.push({path: "../content/Note/MAC_Settings.txt", text: "MAC Settings"});
-notes.push({path: "../content/Note/Oracle.txt", text: "Oracle"});
+notes.push({title: "coding", href: "../content/Note/Coding_Note.txt", text: "Coding Note"});
+notes.push({title: "git", href: "../content/Note/Git_Settings.txt", text: "Git Settings"});
+notes.push({title: "mac", href: "../content/Note/MAC_Install.txt", text: "MAC Install"});
+notes.push({title: "mac", href: "../content/Note/MAC_Settings.txt", text: "MAC Settings"});
+notes.push({title: "oracle", href: "../content/Note/Oracle.txt", text: "Oracle"});
 
 $(function () {
     $("#search-input").keyup(function () {
-        var keyword = $("#search-input").val().toLowerCase();
+        var keyword = $("#search-input").val().trim().toLowerCase();
 
         if (keyword === "") {
             $(".item").each(function (index, element) {
@@ -23,10 +23,13 @@ $(function () {
             });
         }
     });
+    // $(".quick-tag").onclick(function () {
+    //
+    // });
 
     var html = "";
     notes.forEach(function (element) {
-        html += "<div class=\"item\"><a href=\"" + element.path + "\">" + element.text + "</a></div>";
+        html += "<div class=\"item\" title=\"" + element.title + "\"><a href=\"" + element.href + "\">" + element.text + "</a></div>";
     });
 
     $("#main_t").append(html);
