88dfa41bda6e80bd4372e4b47b153d1bdefce66f
Jian-Min-Huang <yfr.huang@hotmail.com>
2017-08-27 23:41:00 +0800

    fix filter function

--- build/bundle.js
+++ build/bundle.js
@@ -377,7 +377,7 @@ $(function () {
             });
         } else {
             $(".item").each(function (index, element) {
-                if ($(element).find("a").text().toLowerCase().indexOf(keyword) != -1) {
+                if ($(element).find("button").text().toLowerCase().indexOf(keyword) != -1) {
                     $(element).show();
                 } else {
                     $(element).hide();
